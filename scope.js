1d6089f30b3d99d2e5c679b01ff30f3444923a96
Min Thant Mg <minthantmg@gicjp.com>
2023-08-08 13:52:04 +0630

    code update

---
+++ scope.js
@@ -0,0 +1,9 @@
+var num = 20; //global
+let count = 10; //block
+const VAL = 70;
+
+num = 30;
+count = 40;
+
+console.log(num)
+console.log(count)
---
+++ str.js
@@ -0,0 +1,13 @@
+let str = "Hello"
+str += "world"
+
+console.log(str)
+
+
+let str2 = "i can\'t speak english"
+ let s = str2.toUpperCase()
+ let result = str2.slice(-1,4)
+
+console.log(s)
+console.log(str2[str2.length - 1])
+console.log(result)
